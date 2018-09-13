c53e097c5a274d8937ed76949b2178f74fab9f5a
Tom Noden <ntprogramation@gmail.com>
2018-09-13 11:23:15 +0200

    Application simple

---
+++ app/src/main/java/com/example/wilder/quetesimpleui/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.wilder.quetesimpleui;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button   mButton;
+    EditText mEdit;
+    EditText mEdit2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mButton = (Button) findViewById(R.id.button);
+        mEdit   = (EditText) findViewById(R.id.editText);
+        mEdit2   = (EditText) findViewById(R.id.editText2);
+
+
+        mButton.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    public void onClick(View view)
+                    {
+
+                        String afficher = mEdit.getText().toString();
+                        String afficher2 = mEdit2.getText().toString();
+
+                        Toast.makeText(MainActivity.this, "Bienvenu"+ afficher + " " + afficher2, Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+        CheckBox mCheck = findViewById(R.id.checkBox);
+        mCheck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean ischecked) {
+                if(ischecked){
+
+                    mEdit.setEnabled(true);
+                    mEdit2.setEnabled(true);
+                }else{
+                    mEdit.setEnabled(false);
+                    mEdit2.setEnabled(false);
+                }
+            }
+        });
+
+    }
+
+
+}
